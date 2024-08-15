3bd4bdbf16c47ad96d10fd89da160885ed69f462
muskit <15199219+muskit@users.noreply.github.com>
2024-08-14 20:34:32 -0700

    add discordgo lib

--- bot.go
+++ bot.go
@@ -4,12 +4,17 @@ import (
 	"log"
 
 	"github.com/joho/godotenv"
+	"github.com/bwmarrin/discordgo"
 )
 
 func RunBot() {
+	// load .env file
 	err := godotenv.Load()
 	if err != nil {
 		// error'd trying to read .env!
 		log.Fatal("could not load .env!")
 	}
+
+	// init bot
+	discord, err := discordgo.New("Bot " + "authentication token")
 }
