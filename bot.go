23cbedabb628bf6c300f07d1543abf8f197ad040
muskit <15199219+muskit@users.noreply.github.com>
2024-08-31 01:42:33 -0700

    add some commands

--- bot.go
+++ bot.go
@@ -4,10 +4,61 @@ import (
 	"log"
 	"os"
 	"os/signal"
+	"strings"
 
 	"github.com/bwmarrin/discordgo"
 	"github.com/joho/godotenv"
 )
+
+var gameChoices = []*discordgo.ApplicationCommandOptionChoice {
+	{
+		Name: "Honkai Impact 3rd",
+		Value: "Honkai Impact 3rd",
+	},
+	{
+		Name: "Genshin Impact",
+		Value: "Genshin Impact",
+	},
+	{
+		Name: "Honkai Star Rail",
+		Value: "Honkai Star Rail",
+	},
+	{
+		Name: "Zenless Zone Zero",
+		Value: "Zenless Zone Zero",
+	},
+}
+
+var optionalGameChoices = []*discordgo.ApplicationCommandOption {
+	{
+		Name: "game_1",
+		Description: "A game to check codes for.",
+		Type: discordgo.ApplicationCommandOptionString,
+		Choices: gameChoices,
+		Required: false,
+	},
+	{
+		Name: "game_2",
+		Description: "A game to check codes for.",
+		Type: discordgo.ApplicationCommandOptionString,
+		Choices: gameChoices,
+		Required: false,
+	},
+	{
+		Name: "game_3",
+		Description: "A game to check codes for.",
+		Type: discordgo.ApplicationCommandOptionString,
+		Choices: gameChoices,
+		Required: false,
+	},
+	{
+		Name: "game_4",
+		Description: "A game to check codes for.",
+		Type: discordgo.ApplicationCommandOptionString,
+		Choices: gameChoices,
+		Required: false,
+	},
+}
 
 var commands = []*discordgo.ApplicationCommand {
 	{
@@ -27,6 +78,111 @@ var commands = []*discordgo.ApplicationCommand {
 			},
 		},
 	},
+	/// CONFIGURATION COMMANDS ///
+	{
+		Name: "subscribe_channel",
+		Description: "Subscribe a channel to automatically announce code activity changes.",
+		Options: []*discordgo.ApplicationCommandOption{
+			{
+				Name: "channel",
+				Description: "The channel to auto-announce codes to.",
+				Type: discordgo.ApplicationCommandOptionChannel,
+				Required: true,
+			},
+			{
+				Name: "game",
+				Description: "Game to subscribe for codes to. Don't specify to announce for all games.",
+				Type: discordgo.ApplicationCommandOptionString,
+				Choices: gameChoices,
+				Required: false,
+			},
+			{
+				Name: "ping_role",
+				Description: "Role to ping when codes (for game if specified) have been updated.",
+				Type: discordgo.ApplicationCommandOptionRole,
+				Required: false,
+			},
+			{
+				Name: "announce_code_removals",
+				Description: "Determines if bot should announce codes being removed. Default: true",
+				Type: discordgo.ApplicationCommandOptionBoolean,
+				Required: false,
+			},
+		},
+	},
+	{
+		Name: "unsubscribe_channel",
+		Description: "Unsubscribe a channel from all code announcements.",
+		Options: []*discordgo.ApplicationCommandOption{
+			{
+				Name: "channel",
+				Description: "The channel to unsubscribe all announcement codes from.",
+				Type: discordgo.ApplicationCommandOptionChannel,
+				Required: true,
+			},
+		},
+	},
+	{
+		Name: "create_embed",
+		Description: "Create an embed that automatically updates with active codes. Shows all games if none are specified.",
+		Options: []*discordgo.ApplicationCommandOption{
+			{
+				Name: "channel",
+				Description: "The channel in which to create the embed.",
+				Type: discordgo.ApplicationCommandOptionChannel,
+				Required: true,
+			},
+			optionalGameChoices[0],
+			optionalGameChoices[1],
+			optionalGameChoices[2],
+			optionalGameChoices[3],
+		},
+	},
+	/// ON-DEMAND RUN COMMANDS ///
+	{
+		Name: "active_codes",
+		Description: "Check the current active codes for MiHoYo games. Shows all games if none are specified.",
+		Options: optionalGameChoices,
+	},
+}
+
+// Command options typedef
+type CMDOptionsMap = map[string]*discordgo.ApplicationCommandInteractionDataOption
+func parseOptions(options []*discordgo.ApplicationCommandInteractionDataOption) (om CMDOptionsMap) {
+	om = make(CMDOptionsMap)
+	for _, opt := range options {
+		om[opt.Name] = opt
+	}
+	return
+}
+
+func interactionAuthor(i *discordgo.Interaction) *discordgo.User {
+	if i.Member != nil {
+		return i.Member.User
+	}
+	return i.User
+}
+
+// EXAMPLE: echo cmd handler
+func handleEcho(s *discordgo.Session, i *discordgo.InteractionCreate, opts CMDOptionsMap) {
+	
+	builder := new(strings.Builder)
+	if v, ok := opts["author"]; ok && v.BoolValue() {
+		author := interactionAuthor(i.Interaction)
+		builder.WriteString("**" + author.String() + "** says: ")
+	}
+	builder.WriteString(opts["message"].StringValue())
+
+	err := s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
+		Type: discordgo.InteractionResponseChannelMessageWithSource,
+		Data: &discordgo.InteractionResponseData{
+			Content: builder.String(),
+		},
+	})
+
+	if err != nil {
+		log.Panicf("could not respond to interaction: %s", err)
+	}
 }
 
 func RunBot() {
@@ -55,7 +211,7 @@ func RunBot() {
 	}
 
 	// EVENT HANDLERS //
-	// Interaction
+	// Bot Interaction
 	session.AddHandler(func(s *discordgo.Session, i *discordgo.InteractionCreate) {
 		// only commands
 		if i.Type != discordgo.InteractionApplicationCommand {
@@ -64,27 +220,39 @@ func RunBot() {
 
 		// "cast" InteractionData to ApplicationCommandInteractionData
 		data := i.ApplicationCommandData()
-		if data.Name == "echo" {
+		options := parseOptions(data.Options)
+
+		log.Printf("%s ran %s\n", interactionAuthor(i.Interaction), data.Name)
+
+		// Command matching
+		switch data.Name {
+		case "echo":
+			handleEcho(s, i, options)
+			break
+		case "active_codes":
 			
 		}
 
 	})
+
 	// Bot ready
 	session.AddHandler(func(s *discordgo.Session, r *discordgo.Ready) {
 		log.Printf("Logged in as %s", r.User.String())
 	})
 
 
-	// Run!
+	// Run with callbacks configured! //
 	err = session.Open()
 	if err != nil {
 		log.Fatalf("could not open session: %s", err)
 	}
 
+	// wait for interrupt
 	sigch := make(chan os.Signal, 1)
 	signal.Notify(sigch, os.Interrupt)
 	<-sigch
 
+	// close session gracefully
 	err = session.Close()
 	if err != nil {
 		log.Printf("could not close session gracefully: %s", err)
