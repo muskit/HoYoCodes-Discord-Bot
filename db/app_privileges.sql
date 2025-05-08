8d0daed4afad4d8cc04aa4da9cc2c991ca4ce01c
Alex <15199219+muskit@users.noreply.github.com>
2025-05-08 13:20:27 -0700

    remove some deployment-specific files

---
+++ db/app_privileges.sql
@@ -0,0 +1,9 @@
+GRANT INSERT ON guild_cfg.* TO 'monty'@'%';
+GRANT SELECT ON guild_cfg.* TO 'monty'@'%';
+GRANT UPDATE ON guild_cfg.* TO 'monty'@'%';
+GRANT DELETE ON guild_cfg.* TO 'monty'@'%';
+
+GRANT SELECT ON scraper.* TO 'monty'@'%';
+GRANT DELETE ON scraper.* TO 'monty'@'%';
+GRANT INSERT ON scraper.* TO 'monty'@'%';
+GRANT UPDATE ON scraper.* TO 'monty'@'%';
--- pull-deploy.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-git pull
-docker-compose down
-docker-compose rm -f
-docker-compose pull
-docker-compose up --build -d
