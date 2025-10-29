d336ab4db7981976ab1234df4a32db2774fd3271
Alex <15199219+muskit@users.noreply.github.com>
2025-10-29 10:25:12 -0700

    try to fix description invalid characters (emojis)

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -9,6 +9,11 @@ import (
 )
 
 func main() {
+	// desc := "30 crystals, one Hi♪ Love Elf♥ trial card (three-day), and 2,888 asterite"
+	// filtered := util.ReplaceNonAlphanumeric(desc)
+	// fmt.Printf("before=%s\nfiltered=%s\n", desc, filtered)
+	// return
+
 	dbgFlag := flag.Bool("debug", false, "enable debug output")
 	flag.Parse()
 
